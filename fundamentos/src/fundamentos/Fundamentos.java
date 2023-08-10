0b79b01a00a3bd1682028d8c73a043284363ca86
Gabriel Fonseca <gabriel.conceicao4@aluno.senai.br>
2023-08-10 15:06:48 -0300

    enviando atividades

--- fundamentos/src/fundamentos/Fundamentos.java
+++ fundamentos/src/fundamentos/Fundamentos.java
@@ -1,31 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package fundamentos;
 
-/**
- *
- * @author Aluno
- */
 public class Fundamentos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // Declaração de variaveis de tipo primitivos
-        String name = "Moises";
+        //Declaração de variáveis de tipos primitivos
+        String nome = "Moisés";
         int idade = 40;
         boolean estudando = true;
         double media = 5.5;
-
+        
         String bairro = new String("Centro");
-
-        // DEclaração de wrapper
-        int nota1 = Integer.parseInt("S");
+        
+        //Declaração de Wrappers
+        int nota1 = Integer.parseInt("30");
         double mediaF = Double.parseDouble("5.5");
         boolean aprovado = Boolean.parseBoolean("true");
     }
-
+    
 }
--- fundamentos/src/fundamentos/multiplasNotas.java
+++ fundamentos/src/fundamentos/multiplasNotas.java
@@ -1,17 +1,19 @@
 package fundamentos;
 
-public class multiplasNotas {
+public class MultiplasNotas {
     public static void main(String[] args) {
-        int resultado = somar(4,8,2,6);
-        System.out.println("Resultado");
-    }
-    public static int somar(int...valores){
-        int resultados = 0;
-        for (int i = 0; i < valores.length; i++) {
-            int valor = valores[i];
-            resultados += valor;
-        }
-        return resultados;
+        int resultado = somar(4, 8, 2, 7);
+        System.out.println("A soma: " + resultado);   
     }
     
+    public static int somar(int... valores) {
+        int resultado = 0;
+        for (int i = 0; i < valores.length; i++) {
+            int valor = valores[i];
+            resultado += valor;
+            
+        }
+        
+        return resultado;
+    }
 }
